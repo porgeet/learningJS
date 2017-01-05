b663ccd9ad562c43bf105529e4d26e57231f93ad
porgeet <peter@designedbypeter.co.uk>
2017-01-05 15:41:55 +0000

    Started the coding coding challenge for the advanced JS section

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -238,6 +238,7 @@ interviewQuestion("teacher")("Doreen");
 //////////////////////////////////////////////////////
 // Lecture: Closures
 
+/*
 function retirement(retirementAge) {
 	var a = " years left until retirement";
 	return function(yearOfBirth){
@@ -285,7 +286,7 @@ var john ={
 			console.log("Hey! What's up? I'm " + this.name + " and I'm " + this.age + " years old. Have a nice " + timeOfDay);
 		}
 	}
-}
+};
 
 var emily ={
 	name: "emily",
@@ -294,7 +295,56 @@ var emily ={
 }
 
 john.presentation("friendly", "afternoon");
-john.presentation.call
+
+john.presentation.call(emily, "formal", "morning");
+
+// john.presentation.apply(emily, ["friendly", "afternoon"]); 
+// will not work as presentation is not expecting an array as an argument
+
+var johnFriendly = john.presentation.bind(john, "friendly");
+
+johnFriendly("morning");
+johnFriendly("night");
+
+
+// bind() creates a copy of the function and allows arguments to be pre-filled
+// in this case the first argument is pre-defined as formal and the presentation()
+// function is stored in emilyFormal with the first argument set
+var emilyFormal = john.presentation.bind(emily, "formal");
+
+// Now we set the second argument
+emilyFormal("afternoon");
+
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+	var arrRes = [];
+	for(i=0; i < arr.length; i++){
+		arrRes.push(fn(arr[i]));
+	}
+	return arrRes;
+}
+
+// Callback function 1
+function calculateAge(el) {
+	return 2016 - el;
+}
+
+// Callback function 2
+function isFullAge(limit, el){
+	return el >= limit;
+}
+
+var ages = arrayCalc(years, calculateAge);
+// A real world example. isFullAge accepts 2 arguments, but when it is passed into
+// arrayCalc() only one is used. So bind() is used to pre-fill values. Although values
+// are evaluated left to right, the second one is the one we want to pre-fill, so the 
+// first value is left as this
+var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20));
+
+console.log(fullJapan);
+console.log(ages);
+*/
 
 
 
@@ -302,6 +352,72 @@ john.presentation.call
 
 
 
+//////////////////////////////////////////////////////
+// CODING CHALLENGE
+
+/*
+--- Let's build a fun quiz game in the console! ---
+
+1. Build a function constructor called Question to describe a question. A question should include:
+a) question itself
+b) the answers from which the player can choose the correct one (choose an adequate data structure here, array, object, etc.)
+c) correct answer (I would use a number for this)
+
+2. Create a couple of questions using the constructor
+
+3. Store them all inside an array
+
+4. Select one random question and log it on the console, together with the possible answers (each question should have a number) (Hint: write a method for the Question objects for this task).
+
+5. Use the 'prompt' function to ask the user for the correct answer. The user should input the number of the correct answer such as you displayed it on Task 4.
+
+6. Check if the answer is correct and print to the console whether the answer is correct ot nor (Hint: write another method for this).
+
+7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
+*/
+
+function question(){
+	var questions = {
+		0: "Is JS badass?", 
+		1: "Is Breaking Bad badass?", 
+		2: "Is the Mondragon the best medic gun?", 
+		3: "Who is the best programmer in the world?"
+	};
+
+	var answers = [
+		["Yes", "No"],
+		["Yes", "No"],
+		["Yes", "No"],
+		["Mark Zuckerberg", "Pete", "Bisqwit"]
+	];
+
+	var rand = Math.floor(Math.random() * 4);
+	console.log(questions.0);
+	console.log(rand);
+
+	
+}
+
+question();
+
+
+
+
+
+
+
+
+/*
+--- Expert level ---
+
+8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
+
+9. Be careful: after Task 8, the game literally never ends. So include the option to quit the game if the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
+
+10. Track the user's score to make the game more fun! So each time an answer is correct, add 1 point to the score (Hint: I'm going to use the power of closures for this, but you don't have to, just do this with the tools you feel more comfortable at this point).
+
+11. Display the score in the console. Use yet another method for this.
+*/
 
 
 
