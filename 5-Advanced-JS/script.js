6bcd406178ab75843f2c2a9ba7741a36206b369d
porgeet <peter@designedbypeter.co.uk>
2017-01-12 14:33:25 +0000

    Watched and understood the solution

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -376,7 +376,7 @@ c) correct answer (I would use a number for this)
 7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
 */
 
-
+/*
 (function(){
 	var Question = function(q){
 		this.q = console.log(q);
@@ -460,14 +460,47 @@ c) correct answer (I would use a number for this)
 	var answer;
 	var question = new Question(questions[random]);
 })();
+*/
 
 
+(function(){function Question(question, answers, correct) {
+	this.question = question;
+	this.answers = answers;
+	this.correct = correct;
+}
 
-	
+Question.prototype.displayQuestion = function(){
+	console.log(this.question);
 
+	for(i=0; i <this.answers.length; i++){
+		console.log(i + ": " + this.answers[i]);
+	}
+}
 
+Question.prototype.checkAnswer = function(ans){
+	if(ans === this.correct){
+		console.log("Correct answer!");
+	} else {
+		console.log("Wrong answer, try again :)");
+	}
+}
 
+var q1 = new Question("Is JavaScript the coolest programming language in the world?", ["Yes", "No"], 0);
 
+var q2 = new Question("What is the name of this course's teacher?", ["John", "Michael", "Jonas"], 2);
+
+var q3 = new Question("What does best describe coding?", ["Boring", "Hard", "Fun", "Tedious"], 2);
+
+var questions = [q1, q2, q3];
+
+var n = Math.floor(Math.random() * questions.length);
+
+questions[n].displayQuestion();
+
+var answer = parseInt(prompt("Please select the correct answer"));
+
+questions[n].checkAnswer(answer);
+})();
 
 
 /*
