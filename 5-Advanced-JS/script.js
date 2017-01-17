0f2ee50d343684dd67a76fd96958c456cbf858ce
porgeet <peter@designedbypeter.co.uk>
2017-01-17 15:00:15 +0000

    completed the expert coding challenge, checked against my code. Annotated Jonas' code to help me understand it

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -462,6 +462,9 @@ c) correct answer (I would use a number for this)
 })();
 */
 
+/*
+//////////////////////////////////////////////////////////////
+// My code
 
 (function(){
 	function Question(question, answers, correct) {
@@ -483,7 +486,7 @@ c) correct answer (I would use a number for this)
 			
 			console.log("Correct answer!");
 			score ++
-			showScore();
+			questions[n].showScore(score);
 			nextQuestion();
 			
 		
@@ -494,16 +497,16 @@ c) correct answer (I would use a number for this)
 		} else {
 		
 			console.log("Wrong answer, try again :)");
-			showScore();
+			this.showScore(score);
 			nextQuestion();
 		
 		}
 		
 	}
 
-	function showScore() {
+	Question.prototype.showScore = function(scr) {
 
-		console.log("Your current score is: "+ score);
+		console.log("Your current score is: " + scr);
 		console.log("---------------------");
 		
 	}
@@ -534,6 +537,91 @@ c) correct answer (I would use a number for this)
 	questions[n].checkAnswer(answer);
 
 })();
+*/
+
+//////////////////////////////////////////////////////////////
+// Jonas' code
+
+
+(function() {
+    function Question(question, answers, correct) {
+        this.question = question;
+        this.answers = answers;
+        this.correct = correct;
+    }
+
+    Question.prototype.displayQuestion = function() {
+        console.log(this.question);
+
+        for (var i = 0; i < this.answers.length; i++) {
+            console.log(i + ': ' + this.answers[i]);
+        }
+    }
+
+    Question.prototype.checkAnswer = function(ans, callback) {
+        var tim;
+        
+        if (ans === this.correct) {
+            console.log('Correct answer!');
+            tim = callback(true);
+        } else {
+            console.log('Wrong answer. Try again :)');
+            tim = callback(false);
+        }
+        
+        this.displayScore(tim);
+    }
+
+    Question.prototype.displayScore = function(score) {
+        console.log('Your current score is: ' + score);
+        console.log('------------------------------');
+    }
+    
+    
+    var q1 = new Question('Is JavaScript the coolest programming language in the world?',
+                          ['Yes', 'No'],
+                          0);
+
+    var q2 = new Question('What is the name of this course\'s teacher?',
+                          ['John', 'Micheal', 'Jonas'],
+                          2);
+
+    var q3 = new Question('What does best describe coding?',
+                          ['Boring', 'Hard', 'Fun', 'Tediuos'],
+                          2);
+    
+    var questions = [q1, q2, q3];
+    
+    function score() {
+        var sc = 0; // sc variable only available in this scope but available in the keepScore var below because of closures
+        return function(correct) {
+            if (correct) {
+                sc++;
+            }
+            return sc; // the value of sc is returned, as is, or amended if fn above mutates it
+        }
+    }
+    var keepScore = score(); // keepScore is the function returned by the score function
+    
+    
+    function nextQuestion() {
+
+        var n = Math.floor(Math.random() * questions.length);
+        questions[n].displayQuestion();
+
+        var answer = prompt('Please select the correct answer.');
+
+        if(answer !== 'exit') {
+            questions[n].checkAnswer(parseInt(answer), keepScore); // check is only run if prompt is not 'exit' the prompt is parsed and score() is passed as a callback
+            
+            nextQuestion();
+        }
+    }
+    
+    nextQuestion(); // function immediately called game logic is performed and questions presented
+    
+})();
+
 
 
 /*
