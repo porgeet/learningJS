cbbc93ba8f3f683aa3d56d738710dc08965d7b1d
porgeet <peter@designedbypeter.co.uk>
2017-01-29 22:28:35 +0000

    clearing input fields, call() function and clarified why newItem is returned

--- 6-Budgety/app.js
+++ 6-Budgety/app.js
@@ -30,7 +30,7 @@ var budgetController = (function(){
 			var newItem, ID;
 			// create new ID
 			if(data.allItems[type].length > 0){
-				ID = data.allItems[type][data.allItems[type].length - 1].id + 1; // not sure why -1 initially, revisit
+				ID = data.allItems[type][data.allItems[type].length - 1].id + 1; // -1 to get last index id
 			} else {
 				ID = 0;
 			}
@@ -46,8 +46,7 @@ var budgetController = (function(){
 			data.allItems[type].push(newItem);
 
 			// return the new element
-			return newItem; // not sure why returned yet
-
+			return newItem; // returns the object from the constructor to be used in  addListItem() in global app controller
 		},
 
 		testing: function(){
@@ -65,7 +64,9 @@ var UIController = (function(){
 		inputType: '.add__type',
 		inputDescription: '.add__description',
 		inputValue: '.add__value',
-		inputButton: '.add__btn' 
+		inputButton: '.add__btn',
+		incomeContainer: '.income__list',
+		expensesContainer: '.expenses__list'
 
 	};
 
@@ -76,6 +77,45 @@ var UIController = (function(){
 				description: document.querySelector(DOMStrings.inputDescription).value,
 				value: document.querySelector(DOMStrings.inputValue).value
 			};
+
+		},
+
+		addListItem: function (obj, type){
+			var html, newHTML, element;		
+			// create HTML string with placeholder text
+
+			
+			if (type === 'income'){
+				element = DOMStrings.incomeContainer;
+				html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+			} else if(type === 'expense'){
+				element = DOMStrings.expensesContainer;
+				html ='<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+			}
+
+			// replace the placeholder text with actual data
+			newHTML = html.replace('%id%', obj.id);
+			newHTML = newHTML.replace('%description%', obj.description);
+			newHTML = newHTML.replace('%value%', obj.value);
+
+			// insert the HTML into the DOM
+			document.querySelector(element).insertAdjacentHTML('beforeend', newHTML);
+
+		},
+
+		clearFields: function(){
+			var fields, fieldsArray;
+
+			fields = document.querySelectorAll(DOMStrings.inputDescription + ', ' + DOMStrings.inputValue);
+
+			fieldsArr = Array.prototype.slice.call(fields);
+
+			fieldsArr.forEach(function(current, index, array){
+				// forEach() loops over an array, callback functions to this accept three args, the current value of the array item, the index of that item and the array itself, in this case fieldsArr
+				current.value = "";
+			});
+
+			fieldsArr[0].focus();
 
 		},
 
@@ -97,6 +137,7 @@ var controller = (function(budgetCtrl, UICtrl){
 
 		document.addEventListener('keypress', function(event){
 
+		// this is the same as forEach() callback function event references the current value, in this case is it keypress 13, enter
 			if(event === 13 || event.which === 13){
 				ctrlAddItem();
 			}
@@ -111,12 +152,15 @@ var controller = (function(budgetCtrl, UICtrl){
 		// 1. get filled input data
 
 		input = UICtrl.getInput();
-		console.log(input);
 
 		// 2. add item to budget controller
 		newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
 		// 3. add the new item to the UI
+		UICtrl.addListItem(newItem, input.type);
+
+		// 4. clear the fields
+		UICtrl.clearFields();
 
 		// 4. calculate the budget
 
@@ -133,8 +177,6 @@ var controller = (function(budgetCtrl, UICtrl){
 })(budgetController, UIController);
 
 controller.init();
-
-// temp comment to force a git change
 
 
 
